b86432d49d264f53ebbc2f4969b82a855da154bf
Arthur Prudente <91214785+arthurprudenteres@users.noreply.github.com>
2023-03-10 13:56:28 -0300

    cardes added

--- src/utilities/dados.js
+++ src/utilities/dados.js
@@ -7,6 +7,9 @@ import CityImg from "../assets/images/city.png"
 import BoyImg from "../assets/images/boy.png"
 import RainyImg from "../assets/images/rainy.png"
 import CodeImg from "../assets/images/code.png"
+import JapanImg from "../assets/images/japan.png"
+import RestaurantImg from "../assets/images/restaurant.png"
+import AirportImg from "../assets/images/airport.png"
 
 export const dados = [
   {
@@ -19,19 +22,19 @@ export const dados = [
   },
   {
     id: 2,
-    image: FireplaceImg,
-    audioSrc: "https://cdn.pixabay.com/download/audio/2021/09/06/audio_8c5b16b7b6.mp3?filename=crackling-fireplace-nature-sounds-8012.mp3",
-    glow: "shadow-[#542417]",
-    barFrom: "from-[#F95708]",
-    barTo: "to-[#700D01]"
+    image: BoyImg,
+    audioSrc: "https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3?filename=lofi-study-112191.mp3",
+    glow: "shadow-[#319999]",
+    barFrom: "from-[#A1B1AF]",
+    barTo: "to-[#319999]"
   },
   {
     id: 3,
-    image: ForestImg,
-    audioSrc: "https://cdn.pixabay.com/download/audio/2021/08/09/audio_6b294070f5.mp3?filename=forest-with-small-river-birds-and-nature-field-recording-6735.mp3",
-    glow: "shadow-[#2C874E]",
-    barFrom: "from-[#0EA85A]",
-    barTo: "to-[#215757]"
+    image: JapanImg,
+    audioSrc: "https://cdn.pixabay.com/download/audio/2023/02/13/audio_90696831ef.mp3?filename=japanese-garden-139092.mp3",
+    glow: "shadow-[#C57170]",
+    barFrom: "from-[#FF3F6D]",
+    barTo: "to-[#FFFBD1]"
   },
   {
     id: 4,
@@ -59,11 +62,11 @@ export const dados = [
   },
   {
     id: 7,
-    image: BoyImg,
-    audioSrc: "https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3?filename=lofi-study-112191.mp3",
-    glow: "shadow-[#319999]",
-    barFrom: "from-[#A1B1AF]",
-    barTo: "to-[#319999]"
+    image: FireplaceImg,
+    audioSrc: "https://cdn.pixabay.com/download/audio/2021/09/06/audio_8c5b16b7b6.mp3?filename=crackling-fireplace-nature-sounds-8012.mp3",
+    glow: "shadow-[#542417]",
+    barFrom: "from-[#F95708]",
+    barTo: "to-[#700D01]"
   },
   {
     id: 8,
@@ -81,4 +84,28 @@ export const dados = [
     barFrom: "from-[#396192]",
     barTo: "to-[#84BDC9]"
   },
+  {
+    id: 10,
+    image: ForestImg,
+    audioSrc: "https://cdn.pixabay.com/download/audio/2021/08/09/audio_6b294070f5.mp3?filename=forest-with-small-river-birds-and-nature-field-recording-6735.mp3",
+    glow: "shadow-[#2C874E]",
+    barFrom: "from-[#0EA85A]",
+    barTo: "to-[#215757]"
+  },
+  {
+    id: 11,
+    image: RestaurantImg,
+    audioSrc: "https://cdn.pixabay.com/download/audio/2022/03/09/audio_c0e35ca16a.mp3?filename=restaurant-sounds-22945.mp3",
+    glow: "shadow-[#FF3577]",
+    barFrom: "from-[#00BDEA]",
+    barTo: "to-[#FF3577]"
+  },
+  {
+    id: 12,
+    image: AirportImg,
+    audioSrc: "https://cdn.pixabay.com/download/audio/2022/03/11/audio_b1bfb89db8.mp3?filename=lax-atmosphere-at-terminal-48640.mp3",
+    glow: "shadow-[#15727B]",
+    barFrom: "from-[#006587]",
+    barTo: "to-[#F4D581]"
+  }
 ]
